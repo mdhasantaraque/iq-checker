3775bf697a605ec6dd8801720a72d6464563d2b5
Md. Hasan Taraque <mtaraque32@gmail.com>
2022-10-10 22:30:49 +0600

    favicon added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>I am here</h1>
+    </div>
+  );
 }
 
 export default App;
