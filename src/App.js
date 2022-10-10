9939fcddbdc0cf1a65b230bba7792449c9b60667
Md. Hasan Taraque <mtaraque32@gmail.com>
2022-10-11 01:12:40 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Blog from "./components/Blog/Blog";
+import Header from "./components/Header/Header";
+
+import Home from "./components/Home/Home";
+import Statistics from "./components/Statistics/Statistics";
+import Topics from "./components/Topics/Topics";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/home",
+      element: <Home></Home>,
+    },
+    {
+      path: "/topics",
+      element: <Topics></Topics>,
+    },
+    {
+      path: "/statistics",
+      element: <Statistics></Statistics>,
+    },
+    {
+      path: "/blog",
+      element: <Blog></Blog>,
+    },
+    {
+      path: "/header",
+      element: <Header></Header>,
+    },
+  ]);
   return (
-    <div className="App">
-      <h1>I am here</h1>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Topics/Topics.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Topics = () => {
+  return (
+    <div>
+      <h1>Its a topics mooorrrrrr gosiph.......</h1>
+    </div>
+  );
+};
+
+export default Topics;
