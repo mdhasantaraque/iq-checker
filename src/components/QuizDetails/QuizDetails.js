8a21e14bac916a42693f33a08b9b714fee0cec7d
Md. Hasan Taraque <mtaraque32@gmail.com>
2022-10-12 16:42:34 +0600

    quiz page update

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -4,10 +4,18 @@ import QuizPage from "../QuizPage/QuizPage";
 
 const QuizDetails = () => {
   const getQuiz = useLoaderData();
-  // console.log(getQuiz.data.questions);
+  // console.log(getQuiz);
   const quizDetails = getQuiz.data.questions;
   return (
     <div>
+      <div className="text-center p-10">
+        <p className="dark:text-violet-600 text-5xl font-bold">
+          {getQuiz.data.name}
+        </p>
+        <p className="dark:text-gray-600 text-2xl font-bold">
+          total question: {getQuiz.data.total}
+        </p>
+      </div>
       {quizDetails.map((quiz) => (
         <QuizPage key={quiz.id} quiz={quiz}></QuizPage>
       ))}
--- src/components/QuizPage/QuizPage.js
+++ src/components/QuizPage/QuizPage.js
@@ -1,42 +1,118 @@
 import React from "react";
-
+import { toast } from "react-toastify";
 const QuizPage = ({ quiz }) => {
-  console.log(quiz);
-  const { correctAnswer, options, question } = quiz;
-
+  // console.log(quiz);
+  // const { name, questions, total } = quiz;
+  const { options, question, id, correctAnswer } = quiz;
   return (
-    <div>
-      <div className="flex items-center mb-4">
-        <input
-          id="default-radio-1"
-          type="radio"
-          value=""
-          name="default-radio"
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
-        <label
-          for="default-radio-1"
-          className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-600"
-        >
-          {correctAnswer}
-        </label>
-      </div>
-      <div className="flex items-center">
-        <input
-          checked=""
-          id="default-radio-2"
-          type="radio"
-          value=""
-          name="default-radio"
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
-        <label
-          for="default-radio-2"
-          className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-600"
-        >
-          {question}
-        </label>
-      </div>
+    <div className="max-w-lg mx-auto my-10  items-center p-6 dark:text-gray-100 rounded-lg border dark:bg-gray-700 border-gray-200 dark:border-gray-700">
+      <fieldset className="mb-5">
+        <legend className="sr-only">{id}</legend>
+        <div className="mb-6">{question}</div>
+        <div className="flex items-center justify-between rounded-lg p-4 dark:bg-gray-600">
+          <div>
+            <div className="flex items-center  mb-4">
+              <input
+                id="country-option-1"
+                onClick={() => {
+                  if (options[0] === correctAnswer) {
+                    toast.success("Correct answer", { autoClose: 750 });
+                  } else {
+                    toast.error("Incorrect answer", { autoClose: 750 });
+                  }
+                }}
+                type="radio"
+                name={id}
+                value="USA"
+                className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+                aria-labelledby="country-option-1"
+                aria-describedby="country-option-1"
+                // checked=""
+              />
+              <label
+                for="country-option-1"
+                className="text-sm font-medium dark:text-gray-100 ml-2 block"
+              >
+                {options[0]}
+              </label>
+            </div>
+            <div className="flex items-center mb-4">
+              <input
+                id="country-option-2"
+                onClick={() => {
+                  if (options[1] === correctAnswer) {
+                    toast.success("Correct answer", { autoClose: 750 });
+                  } else {
+                    toast.error("Incorrect answer", { autoClose: 750 });
+                  }
+                }}
+                type="radio"
+                name={id}
+                value="Germany"
+                className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+                aria-labelledby="country-option-2"
+                aria-describedby="country-option-2"
+              />
+              <label
+                for="country-option-2"
+                className="text-sm font-medium dark:text-gray-100 ml-2 block"
+              >
+                {options[1]}
+              </label>
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center mb-4">
+              <input
+                id="country-option-3"
+                onClick={() => {
+                  if (options[2] === correctAnswer) {
+                    toast.success("Correct answer", { autoClose: 750 });
+                  } else {
+                    toast.error("Incorrect answer", { autoClose: 750 });
+                  }
+                }}
+                type="radio"
+                name={id}
+                value="Spain"
+                className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+                aria-labelledby="country-option-3"
+                aria-describedby="country-option-3"
+              />
+              <label
+                for="country-option-3"
+                className="text-sm font-medium dark:text-gray-100 ml-2 block"
+              >
+                {options[2]}
+              </label>
+            </div>
+            <div className="flex items-center mb-4">
+              <input
+                id="country-option-4"
+                onClick={() => {
+                  if (options[3] === correctAnswer) {
+                    toast.success("Correct answer", { autoClose: 750 });
+                  } else {
+                    toast.error("Incorrect answer", { autoClose: 750 });
+                  }
+                }}
+                type="radio"
+                name={id}
+                value="United Kingdom"
+                className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+                aria-labelledby="country-option-4"
+                aria-describedby="country-option-4"
+              />
+              <label
+                for="country-option-4"
+                className="text-sm font-medium dark:text-gray-100 ml-2 block"
+              >
+                {options[3]}
+              </label>
+            </div>
+          </div>
+        </div>
+      </fieldset>
     </div>
   );
 };
